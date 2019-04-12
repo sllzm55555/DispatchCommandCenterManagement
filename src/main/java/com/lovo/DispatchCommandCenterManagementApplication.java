2f0553f5f60a0136b1cf000527153d8e57e15f92
xiangchuanjiang <1006244280@qq.com>
2019-04-12 15:09:44 +0800

    向川江

---
+++ src/main/java/com/lovo/DispatchCommandCenterManagementApplication.java
@@ -0,0 +1,12 @@
+package com.lovo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DispatchCommandCenterManagementApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DispatchCommandCenterManagementApplication.class, args);
+    }
+}
