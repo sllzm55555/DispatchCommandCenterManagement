c9f255b20d1b94ba629979fcef0d54945a54739c
sllzm55555 <sllzm@outlook.com>
2019-04-17 13:17:38 +0800

    网页

--- src/main/java/com/lovo/config/InterceptorConfig.java
+++ src/main/java/com/lovo/config/InterceptorConfig.java
@@ -9,23 +9,15 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter
 @Component
 public class InterceptorConfig extends WebMvcConfigurerAdapter {
 
-    //添加多个拦截器
+    @Override
     public void addInterceptors(InterceptorRegistry registry) {
-        //注册拦截器
-        registry.addInterceptor(new MyInterceptor()).addPathPatterns("/**");
+        registry.addInterceptor(new MyInterceptor())
+                .addPathPatterns("/**");
     }
 
-    /**
-     * 放过静态资源
-     */
+    @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
-        registry.addResourceHandler("/static/**")
-                .addResourceLocations("classpath:/static/");
-
-        registry.addResourceHandler("/templates/**")
-                .addResourceLocations("classpath:/templates/");
+        registry.addResourceHandler("/staticResources/**")
+                .addResourceLocations("classPath:/staticResources/");
     }
-
-
-
 }
--- src/main/java/com/lovo/entity/UserEntity.java
+++ src/main/java/com/lovo/entity/UserEntity.java
@@ -1,10 +1,10 @@
 package com.lovo.entity;
 
 public class UserEntity {
-    private int id;
 
-    private String sss;
-    private String sssss;
-    private String sssssss;
-    private UserEntity userEntity;
+
+
+
+
+
 }
