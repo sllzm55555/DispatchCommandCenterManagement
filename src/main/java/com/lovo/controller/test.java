8135aec6ef8b0cbc42d91c8f7d2181e5fa31a2b4
jitao123 <1432599195@qq.com>
2019-04-17 13:10:19 +0800

    Merge branch 'master' of E:\Test\DispatchCommandCenterManagement with conflicts.

--- src/main/java/com/lovo/controller/test.java
+++ src/main/java/com/lovo/controller/test.java
@@ -6,6 +6,14 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class test {
 
+
+
+    public void sendMessage(String message){
+
+    }
+//    相当于一个监听器，只要开启以后，这个程序会一直运行，相当于死循环
+
+
     @RequestMapping("start")
     public String start(){
         return "ThymeleafModel";
