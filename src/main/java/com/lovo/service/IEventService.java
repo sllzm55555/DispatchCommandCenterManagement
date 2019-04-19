4208449dc358dce7b6baf7e832f9b0e65f7fb6f9
449718932 <449718932@qq.com>
2019-04-19 14:39:23 +0800

    Merge branch 'master' of F:\JavaWeb\page_work\DispatchCommandCenterManagement with conflicts.

--- src/main/java/com/lovo/service/IEventService.java
+++ src/main/java/com/lovo/service/IEventService.java
@@ -1,14 +1,10 @@
-package com.lovo.dao;
+package com.lovo.service;
 
 import com.lovo.entity.EventEntity;
-import org.springframework.data.domain.Pageable;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.CrudRepository;
 
 import java.util.List;
 
-public interface IEventDao extends CrudRepository<EventEntity,String> {
-
+public interface IEventService {
     /**
      * 带条件查询，实现分页
      * @param eventId 需要条件 事件id
@@ -18,12 +14,6 @@ public interface IEventDao extends CrudRepository<EventEntity,String> {
      * @param pageSize 每页显示多少条
      * @return 返回符合条件的未处理事件集合
      */
-
-    @Query(value = "select * from t_event as e " +
-            "  where if(?1!='',e.event_id = ?1,1=1)" +
-            "  and if(?2!='',e.event_type like %?2% ,1=1) " +
-            "  and if(?3!='',e.event_time>=?3) " +
-            "  limit ?4,?5 ",nativeQuery = true)
     public List<EventEntity> findEventEntitiesByCondition(String eventId,String eventType,String eventTime, int pageNo,int pageSize);
 
     /**
@@ -33,15 +23,7 @@ public interface IEventDao extends CrudRepository<EventEntity,String> {
      * @param eventTime 上报事件时间
      * @return  返回符合条件的总记录条数
      */
-    @Query(value = "select count(*) from t_event as e  where if(?1!='',e.event_id=?1,1=1) " +
-            " and if(?2!='',e.event_type like %?2%,1=1)" +
-            " and if(?3!='',e.event_time>=?3)" ,nativeQuery = true)
     public int  findAllEventEntitiesNumberByCondition(String eventId,String eventType,String eventTime);
 
-    /**
-     *  通过事件Id得到事件对象
-     * @param eventId 事件Id
-     * @return 返回封装好对象的实体
-     */
     public EventEntity findEventByEventId(String eventId);
 }
