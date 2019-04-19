2b62cb4fd22923e12439a10836773f58240dedb3
sllzm55555 <sllzm@outlook.com>
2019-04-19 14:11:25 +0800

    pom文件activemq依赖和websocket依赖

--- src/main/java/com/lovo/testActiveMQ/Consumer.java
+++ src/main/java/com/lovo/testActiveMQ/Consumer.java
@@ -1,5 +1,6 @@
 package com.lovo.testActiveMQ;
 
+import com.lovo.testWebSocket.MyHandler;
 import org.apache.activemq.command.ActiveMQQueue;
 import org.junit.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -7,20 +8,35 @@ import org.springframework.jms.annotation.JmsListener;
 import org.springframework.messaging.handler.annotation.SendTo;
 import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Service;
+import org.springframework.web.socket.TextMessage;
+import org.springframework.web.socket.WebSocketSession;
 
 import javax.jms.Destination;
+import java.io.IOException;
 
 /**
  * @author 王涛
  */
 @Component(value = "consumer")
 public class Consumer {
+//
+//    @Autowired
+//    private MyHandler myHandler;
+
     /**
      * 使用JmsListener配置消费者监听的队列，其中message是接收到的消息,此方法会一直运行相当于一个死循环
      */
     @JmsListener(destination="testQueue")
-    public void receiveQueue(String message) {
+    public void receiveQueue(String message) throws IOException {
         System.out.println("成功接受:" + message);
+
+//        WebSocketSession session = myHandler.getSocketSession();
+//
+//        if (null!=session  && session.isOpen()) {
+//            //回复客户端消息
+//            session.sendMessage(new TextMessage("{'name:'serverMessage','message':'"+message+"'}"));
+//        }
+
     }
 
 
---
+++ src/main/java/com/lovo/testWebSocket/MyHandShake.java
@@ -0,0 +1,42 @@
+package com.lovo.testWebSocket;
+
+import org.springframework.http.server.ServerHttpRequest;
+import org.springframework.http.server.ServerHttpResponse;
+import org.springframework.stereotype.Component;
+import org.springframework.web.socket.WebSocketHandler;
+import org.springframework.web.socket.server.HandshakeInterceptor;
+
+import java.util.Map;
+
+/**
+ * @author 王涛
+ * 实现握手的类
+ */
+@Component
+public class MyHandShake implements HandshakeInterceptor {
+    /**
+     * 握手之前干啥，常用来注册用户信息，绑定 WebSocketSession
+     * @param request
+     * @param response
+     * @param wsHandler
+     * @param attributes
+     * @return
+     * @throws Exception
+     */
+    @Override
+    public boolean beforeHandshake(ServerHttpRequest request, ServerHttpResponse response, WebSocketHandler wsHandler, Map<String, Object> attributes) throws Exception {
+        return true;
+    }
+
+    /**
+     * 握手之后干啥
+     * @param request
+     * @param response
+     * @param wsHandler
+     * @param exception
+     */
+    @Override
+    public void afterHandshake(ServerHttpRequest request, ServerHttpResponse response, WebSocketHandler wsHandler, Exception exception) {
+
+    }
+}
