4753ce8eae6dd9881e4bbd5d07bfe6423c0ecc8b
sllzm55555 <sllzm@outlook.com>
2019-04-25 10:44:57 +0800

    日志模块

--- src/main/java/com/lovo/service/IEventLogService.java
+++ src/main/java/com/lovo/service/IEventLogService.java
@@ -7,24 +7,46 @@ import java.util.List;
 
 /**
  * 事件日志接口
+ *
+ * 刘金林
  */
 public interface IEventLogService {
 
     /**
-     * 分页查找满足情况的所有日志
-     * @param pageable 分页情况
-     * @param eventId 条件中的事件编号
-     * @param operateType 操作类型
+     * 保存事件日志信息
+     * 首先需要的得到事件操作相关信息，
+     * @param eLogId 事件日志id
      * @param operator 操作人
      * @param operateTime 操作时间
+     * @param operateType 操作类型
+     * @param eventId 时间id
      * @return
      */
-    public List<EventLogEntity> showEventLogListPage(Pageable pageable, String eventId, int operateType, String operator, String operateTime);
+    public EventLogEntity saveEventLog(String eLogId, String operator, String operateTime, Integer operateType, String eventId);
+
 
     /**
-     * 保存事件日志信息
-     * @param eventLogEntity
+     * 多条件分页查询，得到满足条件的日志
+     * @param currpage
+     * @param pageSize
+     * @param eventId
+     * @param operateType
+     * @param operator
+     * @param operateTime
+     * @return
      */
-    public EventLogEntity saveEventLog(EventLogEntity eventLogEntity);
+    public List<EventLogEntity> showEventLogByPage(Integer currpage, Integer pageSize, String eventId, Integer operateType, String operator, String operateTime);
+
+
+    /**
+     * 得到满足情况的总页数
+     * @param eventId
+     * @param operateType
+     * @param operator
+     * @param operateTime
+     * @param pageSize
+     * @return
+     */
+    public Integer getCount(String eventId, Integer operateType, String operator, String operateTime, Integer pageSize);
 
 }
--- src/main/java/com/lovo/service/impl/PlanLogServiceImpl.java
+++ src/main/java/com/lovo/service/impl/PlanLogServiceImpl.java
@@ -1,9 +1,63 @@
 package com.lovo.service.impl;
 
+import com.lovo.dao.IPlanLogDao;
+import com.lovo.entity.PlanLogEntity;
 import com.lovo.service.IPlanLogService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service(value = "planLogService")
 public class PlanLogServiceImpl implements IPlanLogService {
 
+    @Autowired
+    private IPlanLogDao planLogDao;
+
+    @Override
+    public PlanLogEntity savePlanLog(String pLogId, String operator, String operateTime, Integer operateType, String planId) {
+        PlanLogEntity planLogEntity = new PlanLogEntity();
+        planLogEntity.setpLogId(pLogId);
+        planLogEntity.setOperator(operator);
+        planLogEntity.setOperateType(operateType);
+        planLogEntity.setOperateTime(operateTime);
+        planLogEntity.setPlanId(planId);
+
+        return planLogDao.save(planLogEntity);
+    }
+
+    @Override
+    public List<PlanLogEntity> showPlanLogByPage(Integer currpage, Integer pageSize, String planId, Integer operateType, String operator, String operateTime) {
+        if (null==planId||"".equals(planId)){
+            planId=null;
+        }
+        if (null==operator||"".equals(operator)){
+            operator=null;
+        }
+        if (null==operateTime||"".equals(operateTime)){
+            operateTime=null;
+        }
+        int index = (currpage - 1)* pageSize;
+        List<PlanLogEntity> planLogList = planLogDao.showPlanLogListByPage(planId, operator, operateTime, operateType, index, pageSize);
+        return planLogList;
+    }
+
+    @Override
+    public Integer getCount(String planId, Integer operateType, String operator, String operateTime, Integer pageSize) {
+        if (null==planId||"".equals(planId)){
+            planId=null;
+        }
+
+        if (null==operator||"".equals(operator)){
+            operator=null;
+        }
+        if (null==operateTime||"".equals(operateTime)){
+            operateTime=null;
+        }
+
+        int count = planLogDao.getAllPlanLogCount(planId,operateType,operator,operateTime);
+        int size = pageSize;
+        int totalPage = (count + size - 1)/size;
+        return totalPage;
+    }
 }
